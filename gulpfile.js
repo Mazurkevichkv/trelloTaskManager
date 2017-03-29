54a5bc7b65f7e68af3e5b5585c96699ca97959b6
zinoviyzubko <zinoviy.zubko@gmail.com>
2017-03-30 00:34:12 +0300

    yoba

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,6 @@
 var gulp = require("gulp");
 
 gulp.task("build", function() {
-    return gulp.src("./src/main/webapp/js.html")
+    return gulp.src("./src/main/webapp/*.html")
         .pipe(gulp.dest("./src/main/resources/public/"));
 });
